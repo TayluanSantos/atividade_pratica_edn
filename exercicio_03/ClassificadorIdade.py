f589b1aefeb48de92d74b0de94a1f8134ebb84d2
TayluanSantos <tayluanjesus@gmail.com>
2025-05-26 21:11:34 -0300

    refactor: refatorando o código

--- exercicio_03/ClassificadorIdade.py
+++ exercicio_03/ClassificadorIdade.py
@@ -8,14 +8,13 @@
 
 idade = int(input("Por favor,digite sua idade: "))
 
-if(idade > 0 and idade <= 12):
-  print("Criança")
-
-elif(idade < 17):
-  print("Adolescente")
-
-elif(idade < 59):
-  print("Adulto")
-
+if idade <= 0:
+    print("Idade inválida.")
+elif idade <= 12:
+    print("Criança")
+elif idade <= 17:
+    print("Adolescente")
+elif idade <= 59:
+    print("Adulto")
 else:
-  print("Idoso")
+    print("Idoso")
